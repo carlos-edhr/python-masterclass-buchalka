bd2e4bc80c964331d3c127721808301b2a7ee289
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-04-30 23:25:33 -0700

    First section added, Python basics

---
+++ 1HelloPython/sequence_operators.py
@@ -0,0 +1,18 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pinging "
+string4 = "for the "
+string5 = "fjords "
+string6 = "My parrot sings"
+
+print(string1 + string2 + string3 + string4 + string5)
+
+print("Hello " * 5)
+
+today = "friday "
+print("day" in today) # True
+print("fri" in today) # True
+print("thur" in today) #false
+print("parrot" in string6) #false
+
+print("My ID is: " + str(1) + ".")
---
+++ 1HelloPython/strings2.py
@@ -0,0 +1,7 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[0:6]) #Norweg
+print(parrot[-4:-2])
+print(parrot[-4:12])
